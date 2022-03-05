a9c35cd89ab3307c9495ce94b3db63c7c21d1bc9
Philip Colllier <redcrown01@gmail.com>
2022-03-05 13:05:14 +0000

    Create getgits.sh

--- getgits.sh
+++ getgits.sh
@@ -12,37 +12,46 @@
 # the function list near the bottom of this script.
 
 # define working directory
-working_dir="/usr/local/src"
+export working_dir="/usr/local/src"
 
 # get the latest release from git repos
 download_last(){
-    cd $3
-    wget "$(lastversion --pre $1 --format assets --filter $2)"
+    wget -P "$3/" "$(lastversion --pre $1 --format assets --filter $2)"
 }
 export -f download_last
 
+update_arc-theme() {
+printf "\n\n...arc-theme..."
+cd "$working_dir"
+[[ -d "$working_dir/arc-theme" ]] \
+    || git clone "https://github.com/horst3180/arc-theme" --depth 1
+cd "$working_dir/arc-theme"
+./autogen.sh --prefix=/usr --with-gnome=3.22
+make install
+}
+
 update_audioprism() {
 printf "\n\n...audioprism..."
-cd "${working_dir}"
-apt install -y libpulse-dev libfftw3-dev libsdl2-dev libsdl2-ttf-dev \
-    libsndfile1-dev libgraphicsmagick++1-dev
-[[ -f "${working_dir}/audioprism" ]] \
-    || git clone "https://github.com/vsergeev/audioprism"
-cd "${working_dir}/audioprism"
-make
+cd "$working_dir"
+apt -o DPkg::Lock::Timeout=-1 install -y libpulse-dev libfftw3-dev \
+    libsdl2-dev libsdl2-ttf-dev libsndfile1-dev libgraphicsmagick++1-dev
+[[ -d "$working_dir/audioprism" ]] \
+    || git clone "https://github.com/vsergeev/audioprism" --depth 1
+cd "$working_dir/audioprism"
+make -j4
 make install
 }
 
 update_fd() {
 printf "\n\n...fd..."
-cd "${working_dir}"
+cd "$working_dir"
 git_repo="sharkdp/fd"
 target_file="fd_.*_amd64.deb"
-dl_dir="${working_dir}/fd"
-[[ -f "${dl_dir}" ]] || mkdir -p $dl_dir
-cd "${dl_dir}"
+dl_dir="$working_dir/fd"
+[[ -d "$dl_dir" ]] || mkdir -p $dl_dir
+cd "$dl_dir"
 download_last $git_repo $target_file $dl_dir
-deb_file="$(ls | grep "$target_file")"
+deb_file="$(find . -maxdepth 1 -name "$(echo "$target_file" | sed 's|\.||')")"
 dpkg -i $deb_file
 # clean up
 rm ./*.deb
@@ -50,159 +59,215 @@ rm ./*.deb
 
 update_fetchproxies() {
 printf "\n\n...fetch-some-proxies..."
-cd "${working_dir}"
-[[ -f "${working_dir}/fetch-some-proxies" ]] \
-    || git clone "https://github.com/stamparm/fetch-some-proxies"
+cd "$working_dir"
+[[ -d "$working_dir/fetch-some-proxies" ]] \
+    || git clone "https://github.com/stamparm/fetch-some-proxies" --depth 1
     # nothing more to do
 }
 
 update_fzf() {
 printf "\n\n...fzf..."
-cd "${working_dir}"
+cd "$working_dir"
 git_repo="junegunn/fzf"
 target_file="fzf-.*-linux_amd64.tar.gz"
-dl_dir="${working_dir}/fzf"
-[[ -f "${dl_dir}" ]] || mkdir -p $dl_dir
-cd "${dl_dir}"
+dl_dir="$working_dir/fzf"
+[[ -d "$dl_dir" ]] || mkdir -p $dl_dir
+cd "$dl_dir"
 download_last $git_repo $target_file $dl_dir
-gz_file="$(ls | grep "$target_file")"
-tar -xf $gz_file
-chown root:root fzf
+gz_file="$(find . -maxdepth 1 -name "$(echo "$target_file" | sed 's|\.||')")"
+tar -xvzf "$gz_file"
+chown -R root:root "$dl_dir"
 # uncomment the symlinker below if needed
-#[[ -f "${dl_dir}/fzf" ]] \
-#    && rm "/usr/local/bin/fzf" \
-#    && ln -s "${dl_dir}/fzf" "/usr/local/bin/fzf"
-
+#ln -sf "$dl_dir/fzf" "/usr/local/bin/fzf"
+#
 # clean up
-rm ./*.gz
+rm "$gz_file"
 }
 
 update_fzproxy() {
 printf "\n\n...fzproxy..."
-cd "${working_dir}"
-[[ -f "${working_dir}/fzproxy" ]] \
-    || git clone "https://git.teknik.io/matf/fzproxy"
+cd "$working_dir"
+[[ -d "$working_dir/fzproxy" ]] \
+    || git clone "https://git.teknik.io/matf/fzproxy" --depth 1
 # uncomment the symlinker below if needed
-#[[ -f "${working_dir}/fzproxy/fzproxy" ]] \
-#    && rm "/usr/local/sbin/fzproxy" \
-#    && ln -s "${working_dir}/fzproxy/fzproxy" "/usr/local/sbin/fzproxy"
+#ln -sf "$working_dir/fzproxy/fzproxy" "/usr/local/sbin/fzproxy"
+}
+
+update_glow() {
+printf "\n\n...glow..."
+cd "$working_dir"
+git_repo="charmbracelet/glow"
+target_file="glow_.*linux_x86_64.tar.gz"
+dl_dir="$working_dir"
+download_last $git_repo $target_file $dl_dir
+gz_file="$(find . -maxdepth 1 -name "$(echo "$target_file" | sed 's|\.||')")"
+mkdir glow_linux_x86_64 || rm -rf glow_linux_x86_64/*
+tar -xvzf $gz_file --directory "$working_dir/glow_linux_x86_64"
+chown -R root:root glow_linux_x86_64
+chmod +x glow_linux_x86_64/glow
+# uncomment the symlinker below if needed
+#ln -sf "$dl_dir/glow_linux_x86_64/glow" "/usr/local/bin/glow"
+# clean up
+rm "$gz_file"
 }
 
 update_i3ass() {
-printf "\n\n...fzproxy..."
-cd "${working_dir}"
-[[ -f "${working_dir}/i3ass" ]] \
-    || git clone "https://github.com/budlabs/i3ass"
+printf "\n\n...i3ass..."
+cd "$working_dir"
+[[ -d "$working_dir/i3ass" ]] \
+    || git clone "https://github.com/budlabs/i3ass" --depth 1
+make -j4
 # uncomment the symlinker below if needed
 #scripts="i3flip i3fyra i3get i3gw i3king i3Kornhe \
 # i3list i3menu i3run i3var i3viswiz i3zen"
 #for item in $scripts; do
-#    rm "/usr/bin/$item"
-#    ln -s "${working_dir}/i3ass/src/$item/$item" "/usr/bin/$item"
+#ln -sf "$working_dir/i3ass/src/$item/$item" "/usr/bin/$item"
 #done
+}
+
+update_lf() {
+printf "\n\n...lf..."
+cd "$working_dir"
+git_repo="gokcehan/lf"
+target_file="lf-linux-amd64.tar.gz"
+dl_dir="$working_dir"
+download_last $git_repo $target_file $dl_dir
+gz_file="$(find . -maxdepth 1 -name "$target_file")"
+mkdir lf_linux_amd64 || rm -rf lf_linux_amd64/*
+tar -xvzf $gz_file --directory "$working_dir/lf_linux_amd64"
+chown -R root:root lf_linux_amd64
+chmod +x lf_linux_amd64/lf
+# uncomment the symlinker below if needed
+#ln -sf "$dl_dir/lf_linux_amd64/lf" "/usr/local/bin/lf"
+# clean up
+rm "$gz_file"
+}
+
+update_lowdown() {
+printf "\n\n...lowdown..."
+cd "$working_dir"
+[[ -d "$working_dir/lowdown" ]] \
+    || git clone "https://github.com/kristapsdz/lowdown" --depth 1
+cd "$working_dir/lowdown"
+./configure
+make -j4
+# uncomment the symlinker below if needed
+#ln -sf "$working_dir/lowdown/lowdown" "/usr/local/bin/lowdown"
 }
 
 update_outline_client() {
 printf "\n\n...Outline-Client..."
-cd "${working_dir}"
+cd "$working_dir"
 git_repo="Jigsaw-Code/outline-client"
 target_file="Outline-Client.AppImage"
 app_dir="/opt/outline"
-[[ -f "$app_dir" ]] || mkdir -p $app_dir
+dl_dir="$working_dir"
+[[ -d "$app_dir" ]] || mkdir -p $app_dir
 download_last $git_repo $target_file $dl_dir
-app_img_file="$(ls | grep "$target_file")"
+app_img_file="$(find . -maxdepth 1 -name "$target_file")"
 chmod +x "$app_img_file"
-mv "$app_img_file" "${app_dir}/$app_img_file"
+mv "$app_img_file" "$app_dir/$app_img_file"
 }
 
 update_picom() {
-printf "\n\n...Python-Pulseaudio-Loopback-Tool..."
-cd "${working_dir}"
-[[ -f "${working_dir}/Python-Pulseaudio-Loopback-Tool" ]] \
-    || git clone "https://github.com/yshui/picom"
-cd "${working_dir}/picom"
+printf "\n\n...picom..."
+cd "$working_dir"
+dl_dir="$working_dir/picom"
+[[ -d "$dl_dir" ]] \
+    || git clone "https://github.com/yshui/picom" --depth 1
+# uncomment the symlinker below if needed
+#ln -sf "/usr/local/bin/meson" "/usr/bin/meson"
+cd "$dl_dir"
 git submodule update --init --recursive
-[[ -f "/usr/bin/meson" ]] || \
-    ln -s "/usr/local/bin/meson" "/usr/bin/meson"
 meson --buildtype=release . build
 ninja -C build
 # uncomment the symlinker below if needed
-#[[ -f "${working_dir}/picom/build/src/picom" ]] \
-#    && rm "/usr/local/bin/picom" \
-#    && ln -s "${working_dir}/picom/build/src/picom" "/usr/local/bin/picom"
+#ln -sf "$working_dir/picom/build/src/picom" "/usr/local/bin/picom"
 }
 
 update_pulseaudioloopback() {
 printf "\n\n...Python-Pulseaudio-Loopback-Tool..."
-cd "${working_dir}"
-[[ -f "${working_dir}/Python-Pulseaudio-Loopback-Tool" ]] \
-    || git clone "https://github.com/alentoghostflame/Python-Pulseaudio-Loopback-Tool"
+cd "$working_dir"
+[[ -d "$working_dir/Python-Pulseaudio-Loopback-Tool" ]] \
+    || git clone "https://github.com/alentoghostflame/Python-Pulseaudio-Loopback-Tool" --depth 1
 # nothing more to do
 }
 
 update_rgpipe() {
 printf "\n\n...rgpipe..."
-cd "${working_dir}"
-[[ -f "${working_dir}/rgpipe" ]] \
-    || git clone "https://github.com/ColonelBuendia/rgpipe"
-chmod +x "${working_dir}/rgpipe/rgpipe"
+cd "$working_dir"
+[[ -d "$working_dir/rgpipe" ]] \
+    || git clone "https://github.com/ColonelBuendia/rgpipe" --depth 1
+chmod +x "$working_dir/rgpipe/rgpipe"
 # uncomment the symlinker below if needed
-#[[ -f "${working_dir}/rgpipe/rgpipe" ]] \
-#    && rm "/usr/local/sbin/rgpipe" \
-#    && ln -s "${working_dir}/rgpipe/rgpipe" "/usr/local/sbin/rgpipe"
+#ln -sf "$working_dir/rgpipe/rgpipe" "/usr/local/sbin/rgpipe"
 }
 
 update_ripgrep() {
 printf "\n\n...ripgrep..."
-cd "${working_dir}"
+cd "$working_dir"
 git_repo="BurntSushi/ripgrep"
 target_file="ripgrep_.*_amd64.deb"
-dl_dir="${working_dir}/ripgrep"
-[[ -f "${dl_dir}" ]] || mkdir -p $dl_dir
-cd "${dl_dir}"
+dl_dir="$working_dir/ripgrep"
+[[ -d "$dl_dir" ]] || mkdir -p $dl_dir
+cd "$dl_dir"
 download_last $git_repo $target_file $dl_dir
-deb_file="$(ls | grep "$target_file")"
+deb_file="$(find . -maxdepth 1 -name "$(echo "$target_file" | sed 's|\.||')")"
 dpkg -i $deb_file
 # clean up
-rm ./*.deb
+rm $deb_file
 }
 
 update_ripgrep_all() {
 printf "\n\n...ripgrep_all..."
-cd "${working_dir}"
+cd "$working_dir"
 git_repo="phiresky/ripgrep-all"
 target_file="ripgrep_all-.*-linux-musl.tar.gz"
-dl_dir="${working_dir}"
+dl_dir="$working_dir"
 download_last $git_repo $target_file $dl_dir
-gz_file="$(ls | grep "$target_file")"
-tar -xf $gz_file && mv ripgrep_all*/ ripgrep-all
-chown -R root:root "${dl_dir}/ripgrep-all"
-chmod +x "${dl_dir}/ripgrep-all/rga"
-chmod +x "${dl_dir}/ripgrep-all/rga-preproc"
+gz_file="$(find . -maxdepth 1 -name "$(echo "$target_file" | sed 's|\.||')")"
+tar -xvzf $gz_file && rm -rf ripgrep-all && mv ripgrep_all*/ ripgrep-all
+chown -R root:root ripgrep-all
+chmod +x ripgrep-all/rga
+chmod +x ripgrep-all/rga-preproc
 # uncomment the symlinker below if needed
-#[[ -f "${dl_dir}/ripgrep-all/rga" ]] && rm "/usr/local/bin/rga"
-#[[ -f "${dl_dir}/ripgrep-all/rga-preproc" ]] && rm "/usr/local/bin/rga-preproc"
-#ln -s "${dl_dir}/ripgrep-all/rga" "/usr/local/bin/rga"
-#ln -s "${dl_dir}/ripgrep-all/rga-preproc" "/usr/local/bin/rga-preproc"
+#ln -sf "$dl_dir/ripgrep-all/rga" "/usr/local/bin/rga"
+#ln -sf "$dl_dir/ripgrep-all/rga-preproc" "/usr/local/bin/rga-preproc"
 # clean up
-rm "${dl_dir}/ripgrep_all*.gz"
+rm "$gz_file"
 }
 
 # update apt repo data
-apt update
+apt -o DPkg::Lock::Timeout=-1 update
 
-# check git repos for updates
-find . -name .git -type d \
-| xargs -n1 -P4 -I% git --git-dir=% --work-tree=%/.. pull origin master
+# use find (unless you already have fd)
+find -type d -name '.git' | xargs -n1 -P4 -I {} \
+    bash -c 'pushd "${0%/*}" \
+    && ( git pull --depth 1; \
+    git tag -d $(git tag -l); \
+    git reflog expire --expire=all --all;
+    git gc --prune=all ) \
+    && popd' {} \;
+
+# use fd if you have it
+#fd -HIFt d '.git' | xargs -n1 -P4 -I {} \
+#    bash -c 'pushd "$0" \
+#    && ( git pull --depth 1; \
+#    git tag -d $(git tag -l); \
+#    git reflog expire --expire=all --all; \
+#    git gc --prune=all ) \
+#    && popd' {}
 
 # compile decoders and tools as separate processes
-list="\
-update_audioprism \
+list="update_audioprism \
 update_fd \
 update_fetchproxies \
 update_fzf \
 update_fzproxy \
+update_glow \
 update_i3ass \
+update_lowdown \
+update_lf \
 update_outline_client \
 update_picom \
 update_pulseaudioloopback \
